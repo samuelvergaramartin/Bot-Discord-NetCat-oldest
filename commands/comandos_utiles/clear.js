0194d793b6a935130ec8e8f755e9a28ed070dedc
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2022-04-02 14:19:10 +0200

    fixed clear.js

--- commands/comandos_utiles/clear.js
+++ commands/comandos_utiles/clear.js
@@ -10,7 +10,11 @@ module.exports = {
 
 //if(message.deletable) message.delete()
    let cantidad = parseInt(args[0]);
+   if(cantidad < 0) return message.channel.send(`:x: **ERROR **: La cantidad introducida no puede ser un número negativo, no puedo borrar ${cantidad} mensajes! no tiene ningún sentido eso!`);
+   if(cantidad > 100) return message.channel.send(":x: **ERROR**: La cantidad de mensajes a borrar no puede ser mayor a 100, si desea borrar más de 100 mensajes utilice este comando varias veces, pero ingresando una cantidad a borrar dentro del rango establecido.");
   if(!cantidad) return message.channel.send("Debes decir una cantidad de mensajes a borrar, debe ser entre 1 y 100.");//.then(msg => msg.delete({timeout: 3000}));
-message.channel.bulkDelete(cantidad);
+message.channel.bulkDelete(cantidad).catch(()=> {
+    message.channel.send(":x: **ERROR**: Dentro del rango de mensajes a borrar existen mensajes con una antigüedad superior a 14 días, no puedo borrar mensajes tan antiguos.\nSi aún así desea borrar todos los mensajes de este canal, le sugiero que use el comando `nc/bomba-atómica`");
+})
     }
 }
