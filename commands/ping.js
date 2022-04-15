23d6fefee637a66915f0d61bd1cfba31682b58b1
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2022-04-15 13:27:01 +0200

    update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,15 +1,22 @@
 module.exports = {
     name: "ping",
     run: async (client, message, args) => {
-  
-      message.channel.send("Midiendo latencia...")
-          const time = Date.now()
-          message.channel.send("Bip...bop...bip...🤖").then(msg => {
-              let tiempoTotal = time - Date.now()
-              var resultado = Math.abs(tiempoTotal);
-              msg.edit("`Discord API:` " +client.ws.ping+ " ms\n`Tiempo de respuesta del bot:` " +resultado+ " ms")
-          })
-  
+        const { MessageEmbed } = require('discord.js');
+        const time = Date.now()
+        const embed = new MessageEmbed()
+        .setTitle("Midiendo latencia...")
+        .setDescription("Discord API: Calculando...\nTiempo de respuesta del bot: Calculando...")
+        .setColor('ff00d9')
+        message.channel.send({ embeds: [embed] }).then(msg => {
+            let tiempoTotal = time - Date.now()
+            var resultado = Math.abs(tiempoTotal);
+            const embed2 = new MessageEmbed()
+            .setTitle("Pong! 🏓")
+            .setDescription("`Discord API:` " +client.ws.ping+ " ms\n`Tiempo de respuesta del bot:` " +resultado+ " ms")
+            .setColor('ff00d9')
+            msg.edit({ embeds: [embed2] });
+        })
+          
     }
   }
   
