00a60eebe77b771b93528aefb962d9e041edf598
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2022-07-28 17:08:10 +0200

    create resume.js

---
+++ commands/public_commands/comandos_de_musica/resume.js
@@ -0,0 +1,20 @@
+const { getVoiceConnection } = require("@discordjs/voice");
+
+module.exports = {
+  name: "resume",
+  description: "Reproducir musica",
+  run: async(client, message, args, discord) =>{
+    const mvc = message.member.voice.channel.id;
+    const pvc = getVoiceConnection(message.guild.id);
+    if (!pvc) return message.reply("No se esta reproduciendo musica");
+
+    if (mvc != pvc.joinConfig.channelId)
+      return message.reply("Tienes que estar en el mismo canal de voz");
+
+    const player = getVoiceConnection(message.guild.id).state.subscription
+      .player;
+
+    player.unpause();
+  },
+};
+
