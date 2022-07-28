e6c540f4c084af86b5a7963535a985fec96f1250
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2022-07-28 15:14:23 +0200

    update index.js

--- index.js
+++ index.js
@@ -140,6 +140,13 @@ for(var archi17 of archivos17) {
     client.comandos.set(comando.name, comando)
 }
 
+let archivos18 = fs.readdirSync('./commands/public_commands/comandos_de_musica').filter((f) => f.endsWith('.js'))
+
+for(var archi18 of archivos18) {
+    let comando = require("./commands/public_commands/comandos_de_musica/" + archi18)
+    client.comandos.set(comando.name, comando)
+}
+
 client.slashcommands = new discord.Collection();
 const slashcommandsFiles = fs.readdirSync('./slashcommands').filter((f) => f.endsWith('.js'));
 
