88c9a0ff5975e13935d8b4ecc2d5369ff46f0854
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2022-07-30 13:40:11 +0200

    update all

--- commands/public_commands/comandos_de_configuracion/setprefix.js
+++ commands/public_commands/comandos_de_configuracion/setprefix.js
@@ -1,31 +1,193 @@
-const Discord = require ('discord.js');
-//const client = new Discord.Client();
-const { Client, MessageEmbed } = require('discord.js');
+const Discord = require('discord.js');
 const db = require('megadb');
-const prefix_db = new db.crearDB('setprefixdb')
+const systemstatus = new db.crearDB('systemstatus');
+const estadosistemadb = require('../../../mega_databases/systemstatus.json');
+
+const estadomodulosdb = require('../../../mega_databases/modulestatusdb.json')
+const estadocomandosdb = require('../../../mega_databases/commandstatusdb.json');
+const color = require('colors');
+const listallcommands = require('../../../sistema/modulos/listado-comandos.json');
+const listallmodules = require('../../../sistema/modulos/modulos.json');
+var configuracion = listallmodules.modulo3;
+var informacion = listallmodules.modulo1;
+var moderacion = listallmodules.modulo2;
+const modconfiguracion = require('../../../sistema/modulos/mod-configuracion.json');
+const modinformacion = require('../../../sistema/modulos/mod-informacion.json');
+const modmoderacion = require('../../../sistema/modulos/mod-moderacion.json');
+
+const comstatus = require('../../../sistema/modulos/com-status-list.json');
+var commandisok = comstatus.green;
+var commandisenmantenimiento = comstatus.yellow;
+var commandiserror = comstatus.red;
+var commandisoffline = comstatus.grey;
+const modstatus = require('../../../sistema/modulos/mod-status-list.json');
+var modisonline = modstatus.online;
+var modisundermaintenance = modstatus.undermaintenance;
+var modisoffline = modstatus.offline;
+var modisoutofservice = modstatus.outofservice;
+const privados = require('../../../sistema/modulos/com-privados.json');
+const socket = require('../../../sistema/sockets/socket.json');
+const { crearDB } = require('megadb');
+var nombrepropietario = socket.ownername;
+var idpropietario = socket.ownerid;
+
+const commandstatusdb = new db.crearDB('commandstatusdb');
+const modulestatusdb = new db.crearDB('modulestatusdb');
+
+var ncomando = listallcommands.setprefix;
+var estadomodulo = estadomodulosdb.configuracion;
+var estadocomando = estadocomandosdb.setprefix;
+var modulodeestecomando = configuracion;
+const prefixdb = new db.crearDB('setprefixdb');
 
 module.exports = {
-    name: "setprefix",
-    run: async (client, message, args) => {
-        var perms = message.member.permissions.has("ADMINISTRATOR")
+    name: `${ncomando}`,
+    run: async(client, message, args) => {
+        var estadosistema = await systemstatus.obtener("mode");
+        function ejecutarcomandoisOK() {
+            
+            
+            var perms = message.member.permissions.has("ADMINISTRATOR")
         
-  if(!perms) return message.channel.send("No tienes suficientes permisos")
+            if(!perms) return message.channel.send("No tienes suficientes permisos")
+          
+            if(!args[0]) return message.channel.send("Debes elejir un prefijo nuevo")
+          
+          
+            prefixdb.establecer(message.guild.id, args[0]).catch(err => { return message.channel.send(err)})
+          
+            
+          
+            message.channel.send(`El prefix ha sido cambiado a **${args[0]}**`)
+          
+}
+        if(ncomando == privados.test) {
+            if(message.author.id !== idpropietario) {
+                message.channel.send(`**⛔ | PERMISSION_DENIED :** No tienes permiso para usar este comando, sólo mi creador puede usar este comando.\nPersonal autorizado: ${nombrepropietario}`);
+            }
+            else {
+                ejecutarcomandoisOK();
+            }
+        }
+        else {
+            if(estadosistema === "under-maintenance") {
+                if(message.author.id !== idpropietario) {
+                    message.channel.send("**:warning: | SYSTEM_UNDER_MAINTENANCE: ** Sistema en mantenimiento, bot en mantenimiento, disculpe las molestias.")
+                }
+                else {
+                    ejecutarcomandoisOK();
+                }
+            }
+            else {
+                if(estadosistema === "online") {
+                    if(estadomodulo == modisonline) {
+                       
+                       if(estadocomando == commandisok) {
+                        ejecutarcomandoisOK();
+                       }
+                       if(estadocomando == commandisenmantenimiento) {
+                        if(message.author.id !== idpropietario) {
+                            message.channel.send(`**:warning: | COMMAND_UNDER_MAINTENANCE :** Comando en mantenimiento, disculpe las molestias.`);
+                        }
+                        else {
+                            ejecutarcomandoisOK();
+                        }
+                       }
+                       if(estadocomando == commandiserror) {
+                        message.channel.send("**:x: | ERROR:** Error detectado! Anti-Crash-System: ACTIVATED!\nPor favor, reporte este error al staff del server o al desarrollador del bot.")
+                       }
+                       if(estadocomando == commandisoffline) {
+                        message.channel.send("**:x: | COMMAND_UNAVALIABLE:** Comando no disponible indefinidamente. Disculpe las molestias.")
+                       }
+                    }
+                    if(estadomodulo == modisundermaintenance) {
+                        if(estadocomando == commandisoffline) {
+                            message.channel.send("**:x: | COMMAND_UNAVALIABLE:** Comando no disponible indefinidamente. Disculpe las molestias.")
+                           }
+                           else {
+                            if(message.author.id !== idpropietario) {
+                                message.channel.send(`**:warning: | MODULE_UNDER_MAINTENANCE :** Módulo de ${modulodeestecomando} en mantenimiento, disculpe las molestias.`);
+                            }
+                            else {
+                                ejecutarcomandoisOK();
+                            }
+                           
+                           }
+                    }
+                    if(estadomodulo == modisoffline) {
+                        if(estadocomando == commandisoffline) {
+                            message.channel.send("**:x: | COMMAND_UNAVALIABLE:** Comando no disponible indefinidamente. Disculpe las molestias.")
+                           }
+                           else {
+                            message.channel.send(`**:x: | MODULE_OFFLINE:** Módulo de ${modulodeestecomando} apagado\nSi desea usar este comando, encienda el módulo.`);
+                           }
+                    }
+                    if(estadomodulo == modisoutofservice) {
+                        if(estadocomando == commandisoffline) {
+                            message.channel.send("**:x: | COMMAND_UNAVALIABLE:** Comando no disponible indefinidamente. Disculpe las molestias.")
+                           }
+                           else {
+                            message.channel.send("**:x: | MODULE_UNAVALIABLE:** Módulo no disponible indefinidamente. Disculpe las molestias.")
+                           }
+                    }
+            
+                }
+                else {
+                    message.channel.send("**:x: | ERROR:** Error detectado! Anti-Crash-System: ACTIVATED!\nPor favor, reporte este error al staff del server o al desarrollador del bot.")
+                }
+            }
+        }
+    }
+}
+var estadosistema = estadosistemadb.mode;
+if(estadosistema === "under-maintenance") {
+    console.log(`Comando: ${ncomando}: ${commandisenmantenimiento} (Motivo: Sistema en mantenimiento)`.yellow);
+}
+else {
+    if(estadosistema === "online") {
+        
+        if(estadomodulo == modisonline) {
+           
+           if(estadocomando == commandisok) {
+            console.log(`Comando: ${ncomando}: ${commandisok}`.green);
+           }
+           if(estadocomando == commandisenmantenimiento) {
+            console.log(`Comando: ${ncomando}: ${commandisenmantenimiento}`.yellow);
+           }
+           if(estadocomando == commandiserror) {
+            console.log(`Comando: ${ncomando}: ${commandiserror}`.red);
+           }
+           if(estadocomando == commandisoffline) {
+            console.log(`Comando: ${ncomando}: ${commandisoffline}`.grey);
+           }
+        }
+        if(estadomodulo == modisundermaintenance) {
+            if(estadocomando == commandisoffline) {
+                console.log(`Comando: ${ncomando}: ${commandisoffline}`.grey);
+               }
+               else {
+                console.log(`Comando: ${ncomando}: ${commandisenmantenimiento} (Motivo: Module: ${modulodeestecomando}: ${modisundermaintenance})`.yellow);
+               }
+        }
+        if(estadomodulo == modisoffline) {
+            if(estadocomando == commandisoffline) {
+                console.log(`Comando: ${ncomando}: ${commandisoffline}`.grey);
+               }
+               else {
+                console.log(`Comando: ${ncomando}: ${commandisoffline} (Motivo: Module: ${modulodeestecomando}: ${modisoffline})`.grey);
+               }
+        }
+        if(estadomodulo == modisoutofservice) {
+            if(estadocomando == commandisoffline) {
+                console.log(`Comando: ${ncomando}: ${commandisoffline}`.grey);
+               }
+               else {
+                console.log(`Comando: ${ncomando}: ${commandisoffline} (Motivo: Module: ${modulodeestecomando}: ${modisoutofservice})`.grey);
+               }
+        }
 
-  if(!args[0]) return message.channel.send("Debes elejir un prefijo nuevo")
-
-
-  prefix_db.establecer(message.guild.id, args[0]).catch(err => { return message.channel.send(err)})
-
-  /*let dueño = message.guild.ownerId;
-  let eldueño = `<@${dueño}>`;
-  let punto = ".";
-  let palabramaestra = "message";
-  let aeldueño = `${palabramaestra}${punto}${eldueño}`;
-  //let dueño = message.guild.fetchOwner();
-  aeldueño.send(`El prefix a cambiado a **${args[0]}**`).catch(err=> { return;})*/
-
-  message.channel.send(`El prefix ha sido cambiado a **${args[0]}**`)
-message.delete()
-//message.channel.send("Comando temporalmente fuera de servicio")
+    }
+    else {
+        console.log(`Comando: ${ncomando}: ${commandiserror}`.red);
     }
 }
