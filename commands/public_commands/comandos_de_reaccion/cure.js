2290591c33b7a6eae8bc4c7b5fd7445cfa919b98
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2022-07-30 12:18:38 +0200

    update cure.js

--- commands/public_commands/comandos_de_reaccion/cure.js
+++ commands/public_commands/comandos_de_reaccion/cure.js
@@ -1,9 +1,12 @@
 const Discord = require ('discord.js');
+const color = require('colors');
+var ncomando = "cure";
 
 module.exports = {
-    name: "cure",
+    name: `${ncomando}`,
     run: async (client, message, args) => {
-    let user = message.mentions.users.first() 
+        message.channel.send("**:x: | ERROR:** Comando averiado, por favor, reporte el error con el comando `nc/bugreport`.");
+    /*let user = message.mentions.users.first() 
         if(!user) return message.channel.send(":x: || Debes mencionar a alguien") || client.users.cache.get(args[0]) || message.author;
         const { MessageEmbed } = require('discord.js');
         const cure = new MessageEmbed()
@@ -12,6 +15,7 @@ module.exports = {
               .setImage("https://media.discordapp.net/attachments/791270423630315521/815681314149761035/Curar.gif")
               .setTimestamp()
               .setFooter({text:`• Hecho por ${message.author.username}`,iconURL: message.author.displayAvatarURL({format: "png"})});
-        message.channel.send({embeds: [cure]}); 
+        message.channel.send({embeds: [cure]}); */
     }
 }
+console.log(`Comando: ${ncomando}: ERROR`.red);
