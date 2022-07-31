5e21de93797dce7598a6d05d0a5c39f1ed45941f
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2022-07-31 18:52:26 +0200

    update system-maintenance.js

--- commands/only_dev_commands/system-maintenance.js
+++ commands/only_dev_commands/system-maintenance.js
@@ -42,6 +42,7 @@ var modulodeestecomando = configuracion;
 module.exports = {
     name: `${ncomando}`,
     run: async(client, message, args) => {
+        var estadosistema = await systemstatus.obtener("mode");
         function ejecutarcomandoisOK() {
             systemstatus.set("mode", "under-maintenance");
             message.channel.send("System Status Mode changed to: under-maintenance");
